f1d35af137b3c4590bae9ac3364495aeee710bdc
Liviu Macsen <liviu@ideveloper.ro>
2019-11-23 21:14:02 +0200

    no message

--- src/ios/CordovaDownloaderPlugin.m
+++ src/ios/CordovaDownloaderPlugin.m
@@ -1,7 +1,7 @@
 #import "CordovaDownloaderPlugin.h"
 #import "CDVFile.h"
 #import "SDNetworkActivityIndicator.h"
-#import "FKDownloadManager.h"
+#import "FKDownloader.h"
 
 @interface CordovaDownloaderPlugin()
 
@@ -15,10 +15,21 @@
 - (void)downloadFile:(CDVInvokedUrlCommand*)command {
     NSLog(@"DOWNLOAFILE!!!");
     
+    NSString *url = [command.arguments objectAtIndex:0];
+    NSString *filePath = [command.arguments objectAtIndex: 2];
+    
+    
     [[SDNetworkActivityIndicator sharedActivityIndicator] startActivity];
     
     @try {
+        NSArray *urls = [NSArray arrayWithObject:url];
         
+        [[FKDownloadManager manager] addTasksWithArray:urls.copy];
+        [[FKDownloadManager manager ] start:url];
+        [[FKDownloadManager manager] acquire:url].delegate = self;
+        [[FKDownloadManager manager ] acquire:url].progressBlock = ^(FKTask *task) {
+            NSLog(@"progress changed %.6f - %@", task.progress.fractionCompleted, task.bytesPerSecondSpeedDescription);
+        };
         
         [[SDNetworkActivityIndicator sharedActivityIndicator] stopActivity];
     } @catch (NSException *exception) {
