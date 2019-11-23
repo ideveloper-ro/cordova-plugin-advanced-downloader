bff9d9d80bd7f938982b4e57c15912ef0741401f
Liviu Macsen <liviu@ideveloper.ro>
2019-11-23 18:38:40 +0200

    no message

--- www/advanced-downloader.js
+++ www/advanced-downloader.js
@@ -2,6 +2,9 @@
  * A native Downloader Plugin for Cordova / PhoneGap.
  */
 
+var exec = require('cordova/exec');
+
+
 module.exports = {
 
 	downloadFile: function(url, filePath, success, failure) {
