c36b9ca60d780c610280be0e40585c62bd4b9fe6
Liviu Macsen <liviu@ideveloper.ro>
2019-11-23 20:33:20 +0200

    no message

--- src/ios/CordovaDownloaderPlugin.m
+++ src/ios/CordovaDownloaderPlugin.m
@@ -1,5 +1,7 @@
 #import "CordovaDownloaderPlugin.h"
 #import "CDVFile.h"
+#import "SDNetworkActivityIndicator.h"
+#import "FKDownloadManager.h"
 
 @interface CordovaDownloaderPlugin()
 
@@ -12,6 +14,16 @@
 
 - (void)downloadFile:(CDVInvokedUrlCommand*)command {
     NSLog(@"DOWNLOAFILE!!!");
+    
+    [[SDNetworkActivityIndicator sharedActivityIndicator] startActivity];
+    
+    @try {
+        
+        
+        [[SDNetworkActivityIndicator sharedActivityIndicator] stopActivity];
+    } @catch (NSException *exception) {
+        [[SDNetworkActivityIndicator sharedActivityIndicator] stopActivity];
+    }
 }
 
 @end
