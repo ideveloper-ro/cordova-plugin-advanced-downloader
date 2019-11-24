6de6796ebccc5fec28184d6b8ffb7ac4309e0696
Liviu Macsen <liviu@ideveloper.ro>
2019-11-24 22:33:43 +0200

    no message

--- src/ios/CordovaDownloaderPlugin.m
+++ src/ios/CordovaDownloaderPlugin.m
@@ -3,7 +3,7 @@
 #import "SDNetworkActivityIndicator.h"
 #import "FKDownloader.h"
 
-@interface CordovaDownloaderPlugin()
+@interface CordovaDownloaderPlugin() <FKTaskDelegate>
 
 @end
 
@@ -13,28 +13,65 @@
 }
 
 - (void)downloadFile:(CDVInvokedUrlCommand*)command {
-    NSLog(@"DOWNLOAFILE!!!");
+    self.callbackId = command.callbackId;
     
     NSString *url = [command.arguments objectAtIndex:0];
-    NSString *filePath = [command.arguments objectAtIndex: 2];
+    NSString *filePath = [command.arguments objectAtIndex:1];
+    if ([filePath hasPrefix:@"file://"]) {
+        filePath = [filePath substringFromIndex:7];
+    }
     
-    
-    [[SDNetworkActivityIndicator sharedActivityIndicator] startActivity];
+    [[_SDNetworkActivityIndicator sharedActivityIndicator] startActivity];
     
     @try {
-        NSArray *urls = [NSArray arrayWithObject:url];
+//        NSArray *urls = [NSArray arrayWithObject:url];
         
-        [[FKDownloadManager manager] addTasksWithArray:urls.copy];
-        [[FKDownloadManager manager ] start:url];
+        NSArray *tasks = [NSArray arrayWithObject:@{FKTaskInfoURL: url,
+            FKTaskInfoFileName: [[url lastPathComponent] stringByDeletingPathExtension],
+            FKTaskInfoVerificationType: @(VerifyTypeMD5),
+            FKTaskInfoVerification: @"5f75fe52c15566a12b012db21808ad8c",
+            FKTaskInfoRequestHeader: @{},
+            FKTaskInfoTags: @[@"group_task_01"],
+            FKTaskInfoSavePath: [filePath stringByAppendingString:@"/file"],
+            FKTaskInfoResumeSavePath: [filePath stringByAppendingString:@"/resume"]
+        }];
+        
+//        NSLog(@"%@", [FKDownloadManager manager].configure.savePath);
+//        NSLog(@"%@", [FKDownloadManager manager].configure.resumeSavePath);
+//        NSLog(@"%@", [filePath stringByAppendingString:@"/file"]);
+        
+        [[FKDownloadManager manager] addTasksWithArray:tasks.copy];
+        
+        [[FKDownloadManager manager] acquire:url].savePath = filePath;
         [[FKDownloadManager manager] acquire:url].delegate = self;
-        [[FKDownloadManager manager ] acquire:url].progressBlock = ^(FKTask *task) {
+
+        [[FKDownloadManager manager] acquire:url].progressBlock = ^(FKTask *task) {
             NSLog(@"progress changed %.6f - %@", task.progress.fractionCompleted, task.bytesPerSecondSpeedDescription);
+            
+            NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
+            [dictionary setObject:task.bytesPerSecondSpeedDescription forKey:@"speed"];
+            [dictionary setObject:task.estimatedTimeRemainingDescription forKey:@"eta"];
+            [dictionary setObject:[NSString stringWithFormat:@"%.6f", task.progress.fractionCompleted] forKey:@"percentage"];
+            
+            
+            CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsDictionary:dictionary];
+            [pluginResult setKeepCallbackAsBool:YES];
+            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
         };
-        
-        [[SDNetworkActivityIndicator sharedActivityIndicator] stopActivity];
+        [[FKDownloadManager manager] start:url];
+
+        [[_SDNetworkActivityIndicator sharedActivityIndicator] stopActivity];
     } @catch (NSException *exception) {
-        [[SDNetworkActivityIndicator sharedActivityIndicator] stopActivity];
+        [[_SDNetworkActivityIndicator sharedActivityIndicator] stopActivity];
     }
+}
+
+
+- (void)downloader:(FKDownloadManager *)downloader didFinishTask:(FKTask *)task {
+    NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
+    [dictionary setObject:[NSString stringWithFormat:@"file://%@", task.filePath] forKey:@"file"];
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:dictionary];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:self.callbackId];
 }
 
 @end
